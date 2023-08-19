bc5b1e769ffdfec0f1e5391476551c010eedcecc
Bryan McMurry <bryanmcmurry7@gmail.com>
2023-08-19 13:30:11 -0500

    ready to pull

--- app/views.py
+++ app/views.py
@@ -868,46 +868,6 @@ class PaymentSuccessView(View):  # Use the View class
         return render(request, self.template_name)
 
 
-# def PaymentSuccess(request):
-#     stripe.api_key = settings.STRIPE_SECRET_KEY
-#     checkout_session_id = request.GET.get("session_id", None)
-#     session = stripe.checkout.Session.retrieve(checkout_session_id)
-
-#     customer = stripe.Customer.retrieve(session.customer)
-#     user = request.user  # Fix: Removed duplicated line
-
-#     # Retrieve the logged-in tenant
-#     logged_in_tenant = Tenant.objects.get(linkToBuiltinUser=user)
-
-#     custom_amount = None
-#     print(session)
-#     print(session.line_items)
-#     for line_item in session.line_items.data:
-#         if line_item.price:
-#             custom_amount = (
-#                 float(line_item.price.unit_amount) / 100
-#             )  # Convert from cents
-#             # break
-
-#     if custom_amount is not None:
-#         lease = logged_in_tenant.linkToLease
-
-#         if lease:
-#             user_payment, created = TenantPayment.objects.get_or_create(
-#                 app_user=logged_in_tenant
-#             )
-#             user_payment.payment_bool = True
-#             user_payment.payment_amount = custom_amount
-#             user_payment.linked_lease = lease
-#             user_payment.stripe_checkout_id = checkout_session_id
-#             user_payment.save()
-
-#             lease.currentBalance += custom_amount
-#             lease.save()
-
-#     return render(request, "payment_success.html", {"customer": customer})
-
-
 def PaymentFail(request):
     return render(request, "payment_fail.html")
 
